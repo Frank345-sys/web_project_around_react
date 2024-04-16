cf6cca13dbe752dda03c7b6c604a714721e7768e
Frank345-sys <francgonzalez456@gmail.com>
2024-04-15 19:27:25 -0600

    Proyecto Sprint 12 Terminado, continuación del Sprint 11, correcciones parte 16

--- src/components/Main.js
+++ src/components/Main.js
@@ -50,20 +50,6 @@ function Main({
     }
   }, [currentUser]);
 
-  //Mostrar cartas
-  const [items, setItems] = useState(cards.slice(0, 3)); // Muestra inicialmente las primeras 3 cards
-  const [hasMore, setHasMore] = useState(true);
-
-  // Función para cargar más items al hacer scroll
-  const fetchMoreData = () => {
-    // Simula una carga de más datos, aquí debes cargar los siguientes 3 items
-    setTimeout(() => {
-      const nextItems = cards.slice(items.length, items.length + 3);
-      setItems([...items, ...nextItems]);
-      setHasMore(nextItems.length > 0); // Indica si hay más items para cargar
-    }, 1500); // Simula un tiempo de carga, ajusta según tu lógica de carga de datos
-  };
-
   // EditModal
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
@@ -275,6 +261,30 @@ function Main({
 }
 
 export default Main;
+
+/*
+{isLoadCards ? (
+            <div className={`content-photos`}>
+              {cards.map((item) => (
+                <Card
+                  key={item._id}
+                  name={item.name}
+                  link={item.link}
+                  idCard={item._id}
+                  likes={item.likes}
+                  user={item.owner}
+                  onLike={onLikeCard}
+                  onDisLike={onDisLikeCard}
+                  onOpenPopUpImage={openPopUpImage}
+                  onOpenModalDeleteCard={openModalDeleteCard}
+                ></Card>
+              ))}
+            </div>
+          ) : (
+            <ReactLoading type={"bars"} color="#f4f4f4" />
+          )}
+*/
+
 //${isLoadCards ? "" : "shimmer"}
 /*
 {cards.map((item) => (
